76cf1515c47701fe26a953e7b77e0c2d54c96fb9
YeMyat Thein <yemyatthein@gmail.com>
2015-08-02 03:10:45 +0100

    Use DB from MongoLab

--- web/app.py
+++ web/app.py
@@ -2,32 +2,46 @@ from flask import Flask, jsonify, request, render_template
 from itertools import count
 from datetime import datetime
 import time
-import threading
+from copy import deepcopy
 import json
+from pymongo import MongoClient
 
 
-def load(path):
-    result = {}
-    last = 0
-    try:
-        with open(path, 'r') as f:
-            for l in f:
-                d = json.loads(l)
-                result[d['id']] = d
-                last = d['id']
-    except IOError:
-        pass
-    
-    return result, last + 1;
+username = None
+password = None
 
+lines = []
+with open('../auth.data', 'r') as f:
+    for l in f:
+        lines.append(l[:-1])
 
-lock = threading.Lock()
+assert len(lines) == 2
+
+username = lines[0]
+password = lines[1]
+
+connection = MongoClient("ds041150.mongolab.com", 41150)
+db = connection["excerptdb_ymt"]
+db.authenticate(username, password)
+
+user_db = db.users
+excerpt_db = db.excerpts
+
 app = Flask(__name__)
-data_file = './store.db'
 
-memory, start = load(data_file)
+start = excerpt_db.count() + 10
 next_index = count(start)
 
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
 
 @app.route('/')
 def index():
@@ -40,46 +54,46 @@ def save():
     now = time.mktime(now.timetuple())
     
     d = dict(request.form)
-    d['created_time'] = now
-    d['id'] = next_index.next()
-    d['user'] = 'yemyat89'
     
-    memory[d['id']] = d
-
-    with lock:
-        with open(data_file, 'a') as f:
-            j = json.dumps(d)
-            f.write(j)
-            f.write('\n')
+    excerpt_data = {'about': d['about'][0],
+                    'url': d['url'][0],
+                    'title': d['title'][0],
+                    'excerpt': d['excerpt'][0]}
+    excerpt_data['created_time'] = now
+    excerpt_data['id'] = next_index.next()
+    
+    excerpt_data['username'] = 'yemyat89'
+    
+    excerpt_db.insert(excerpt_data)
 
     return jsonify(dict(result=True))
 
 
 @app.route('/view')
 def view():
-    return jsonify(dict(data=memory))
+    d = [x for x in excerpt_db.find()]
+    for x in d:
+        x.pop('_id')
+    return jsonify(dict(data=d))
 
 
 @app.route('/list_excerpts')
 def list_excerpts():
-    mem = memory.copy()
     timestamps = {}
+    mem2 = [x for x in excerpt_db.find()]
     
-    for k1 in mem:
-        for k2 in mem[k1]:
-            d = mem[k1][k2]
-            if isinstance(d, list):
-                mem[k1][k2] = d[0]
-            elif isinstance(d, float):
-                g = datetime.fromtimestamp(d)
-                mem[k1][k2] = g.strftime('%d/%m/%Y %H:%M')
-                timestamps[k1] = d
+    for i, x in enumerate(mem2):
+        for k, v in x.iteritems():
+            if isinstance(v, float):
+                g = datetime.fromtimestamp(v)
+                mem2[i][k] = g.strftime('%d/%m/%Y %H:%M')
+                timestamps[i] = v
     
-    return render_template('list.html', data=mem, ts=timestamps)
+    return render_template('list.html', data=mem2, ts=timestamps)
 
 @app.route('/detail_excerpt/<rid>')
 def detail(rid):
-    d = memory[int(rid)]
+    d = excerpt_db.find_one({'id': int(rid)})
     return render_template('detail_excerpt.html', d=d) 
 
 if __name__ == '__main__':
